bd4e2674d48ce0c0c34650338233369a3d57b67f
Rodrigo Ariel Castelo <rodrigocastelo30@gmail.com>
2022-07-28 23:56:12 -0300

    Lista de alumnos

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  alumnos =[
+    {
+      nombre:'Rodrigo',
+      apellido:'Castelo'
+    },
+    {
+      nombre:'Reinaldo',
+      apellido:'Castelo'
+    },
+    {
+      nombre:'Nora',
+      apellido:'Gomez'
+    },
+    {
+      nombre:'Antonella',
+      apellido:'Castelo'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
